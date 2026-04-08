12656825688b69b67e2aa3e709818dad188d6747
Aaron2903 <aaronfranksanjay@gmail.com>
2026-04-08 22:48:00 +0530

    Add files via upload

---
+++ src/main/java/com/stockmarket/dto/TradeRequest.java
@@ -0,0 +1,17 @@
+package com.stockmarket.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class TradeRequest {
+
+    @NotBlank(message = "Stock symbol is required")
+    private String symbol;
+
+    @NotNull(message = "Quantity is required")
+    @Min(value = 1, message = "Quantity must be at least 1")
+    private Integer quantity;
+}
